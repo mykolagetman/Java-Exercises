65ae221de3d41352f1f56844dbe7bc376e73de86
Mykola Getman <mykolagetman@hotmail.com>
2018-11-01 10:20:25 +0100

    Exercises

--- Tema 2/Ejercicio4T2.java
+++ Tema 2/Ejercicio4T2.java
@@ -1,14 +1,15 @@
 public class Ejercicio4T2 {
   public static void main(String args[]) {
     
-    //String conversor;
-    int euros = 1;
+    //Cnversor euros a pesetas
+    
+    int euros = 50;
     double pesetas = 166.386;
     double resultado;
     
-    euros = pesetas;
+    resultado = euros * pesetas;
     
-    System.out.println("La conversion es:" + resultado);
+    System.out.println("La conversion de "+ euros +" es igual a: " + resultado);
     
   }
 }
