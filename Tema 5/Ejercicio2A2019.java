2cbef1e35085b66b2eebf1cc46a433edf896b4d5
Mykola Getman <mykolagetman@hotmail.com>
2019-10-16 08:44:12 +0200

    Actualizacion

---
+++ Tema 5/Ejercicio2A2019.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Ejercicio2A2019 {
+  public static void main(String args[]) {
+    Scanner x = new Scanner(System.in);
+    
+   int i = 0;
+   
+   while (i < 101) {
+     System.out.println(i);
+     i = i + 5;
+   }
+  }
+}
+
